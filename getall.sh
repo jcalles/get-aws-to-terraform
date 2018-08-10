c5acf14a2b7bb52ec3f5ca56f08fec41cb1e29b1
Javier Calles <javiercalles@gmail.com>
2018-08-10 11:20:05 -0500

    Update getall.sh

--- getall.sh
+++ getall.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
+## support to getsession.sh 
 
-terraformget(){
-	echo '---------------------------'	
-	echo "Creating dir configurations from profile: aws-${PROFILE}"
-				echo "getting new config please wait..."
+set -ex
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_REGION=${AWS_DEFAULT_REGION}
+
+export AWS_PROFILE=${AWS_PROFILE}
+export AWS_ROLE=${AWS_ROLE}
+
+if [ -z "${AWS_ROLE}" ] || [ -z "${AWS_PROFILE}" ]
+then
+echo "ROLE or PROFILE does not exist!"
+fi
+
+if !  which aws > /dev/null; then
+			echo '---------------------------'	
+				echo "AWS Cli is not installed"
 				echo '---------------------------'	
-	mkdir -p aws-${PROFILE} && cd aws-${PROFILE}
-	
-	terraforming help | grep terraforming | grep -v help | awk -v var="${PROFILE}" '{ print "terraforming",  $2 ," --profile", var  " > ",  $2"-" var".tf" }' |bash 2> /dev/null
-		# find files that only have 1 empty line (likely nothing in AWS)
-		echo '---------------------------'	
-		echo "deleting empty tf files"
-		echo '---------------------------'	
-		
-		#find $(pwd) -type f -name '*.tf' | xargs wc -l | grep ' 1 .'|xargs rm -f
-		find $(pwd) -type f -name '*.tf' -empty -delete
-		cd ../
-}
+				exit 1
+fi
 
- if ! which gem > /dev/null ; then
+if ! which gem > /dev/null ; then
  echo '---------------------------'	
 				echo "ruby gem  is not installed"
 				echo "please install ruby gem"
@@ -26,66 +29,77 @@ terraformget(){
 				exit 1
 fi
 
-
-#####################################################################################
-if [ -z $1 ]
-	then
-		echo '---------------------------'	
-		echo "Profile not present."
-		echo "Useage $0 [profile]"
-		echo "Enter  a valid profile"
-		echo '---------------------------'	
-			if !  which aws > /dev/null; then
-			echo '---------------------------'	
-				echo "AWS Cli is not installed"
-				echo '---------------------------'	
-				exit 1
-			else	
-			echo '---------------------------'	
-				echo "Profile is:"
-				cat ~/.aws/credentials|egrep -v "aws_*|=" | tr '[' ' '|tr ']' ' '|awk /./
-			echo '---------------------------'	
-			fi
-else
-	PROFILE=$1
-	## Terraforming is on https://github.com/dtan4/terraforming#supported-version
-		if !  which terraforming > /dev/null; then
+if !  which terraforming > /dev/null; then
 		echo '---------------------------'	
 			echo "Terraforming is not installed"
 			echo '---------------------------'	
-			gem install terraforming
-		else
-			if ! [ -d aws-${PROFILE} ]
-			then 
-								
-				terraformget
-				ls aws-${PROFILE}
-				
-			else
-			echo '---------------------------'	
-			echo "profile:  aws-${PROFILE} exist!!"
-				echo "moving profile aws-${PROFILE} to aws-${PROFILE}.old"
-				echo '---------------------------'	
-				if [ -d aws-${PROFILE}.old ]
-				then
-				echo '---------------------------'	
-					echo "deleting old conf from aws-${PROFILE}"
-					echo '---------------------------'	
-					rm -Rf aws-${PROFILE}.old
-				fi
-					mv aws-${PROFILE} aws-${PROFILE}.old
-					echo '---------------------------'	
-					echo "Listing old aws configurations"
-					echo '---------------------------'	
-					ls aws-${PROFILE}.old/*
-					terraformget
-					echo '---------------------------'	
-					echo "Listing NEW aws configurations"
-					echo '---------------------------'	
-					ls aws-${PROFILE}
-			
-			fi
-		
-		
-		fi
+			sudo -u ${USER} gem install terraforming
+fi	
+if ! [ -f ~/.aws/credentials ]
+then
+	echo "credentials files does not exist!"
+else
+	echo '---------------------------'
+	echo "PROFILES are:"
+	cat  ~/.aws/credentials|egrep -v "aws_*|=" | tr '[' ' '|tr ']' ' '|awk /./
+	echo '---------------------------'
 fi
+
+if ! [ -f ~/.aws/account-roles ]
+then
+	echo "File with Roles does not exist"
+else
+	echo "ROLES are:"
+	cat ~/.aws/account-roles|egrep -v "aws_*|=" | tr '[' ' '|tr ']' ' '|awk /./
+fi
+
+
+if [ -z "${1}" ]
+	then
+		echo '---------------------------'	
+		echo "Usage $0 PATH"
+		echo "Enter  PATH to save tfs"
+	exit 1
+	
+	else
+		mkdir -p aws-${AWS_PROFILE}
+fi
+
+AWS_PROFILE="${1}"
+
+emptyfiles(){
+	if [ -z ${AWS_PROFILE} ]
+	then
+		echo "AWS_PROFILE var is impty"
+		exit 1
+	
+	fi
+		# find files that only have 1 empty line (likely nothing in AWS)
+		#find $(pwd) -type f -name '*.tf' | xargs wc -l | grep ' 1 .'|xargs rm -f
+	if  [ -d aws-${AWS_PROFILE} ]
+	then
+		echo '---------------------------'	
+		echo "finding empty tf files"
+		echo '---------------------------'	
+		find aws-${AWS_PROFILE} -type f -name '*.tf' -empty -delete
+	else
+		echo "PATH does not  exist"	
+	fi
+
+}
+
+terraformget(){
+	echo '---------------------------'	
+	echo "searching dir configurations from profile: aws-${AWS_PROFILE}"
+	echo "getting new config please wait..."
+	echo '---------------------------'	
+	
+	#terraforming help | grep terraforming | grep -v help | awk -v var="${AWS_PROFILE}" '{ print "terraforming",  $2 , " > ",  $2"-" var".tf" }' |bash 2> /dev/null
+	terraforming help | grep terraforming | grep -v help | awk -v var="aws-${AWS_PROFILE}" '{ print "terraforming",  $2 , " > ",  var"/"$2".tf" }'|bash 2> /dev/null
+	
+}
+
+#####################################################################################
+emptyfiles
+terraformget
+emptyfiles
